ff99c9a2c49383e87f64cc8a31fa76bffe25c513
phillips-jordan <jordan.phillips.nb@gmail.com>
2018-05-04 12:27:13 -0400

    first commit

---
+++ backend/server.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.raw({type:"*/*"}))
+
+let serverState = {
+    messages:[],
+    users:[],
+    livesession: []
+}
+
+app.get('/activeUsers', (req, res)=>{
+    let active = []
+    let stamp = new Date().getTime();
+    active = serverState.messages.map(x=>{
+        if(x.time>stamp-300000){
+            return x.username
+        }
+        return null
+    })
+    active = active.filter(x=>{
+        return x!==null
+    })
+    let uniquer = active.filter((x, i)=>{
+        return active.indexOf(x)===i
+    })
+    
+    res.send(JSON.stringify(uniquer))
+})
+
+app.post('/create', (req,res)=> {
+    let parsedUser = JSON.parse(req.body.toString())
+     if (!serverState.users[parsedUser.username]) {
+       serverState.users[parsedUser.username] = parsedUser.password;
+       res.send('success')
+     }
+     else {
+         res.send('failure')
+     }
+})
+
+app.post("/login", (req, res)=> {
+    let parsedUser = JSON.parse(req.body.toString())
+    if (serverState.users[parsedUser.username] === parsedUser.password){
+        let sessionID = Math.floor(Math.random()*10000000)
+        serverState.livesession[parsedUser.username]=sessionID
+        console.log(serverState)
+        let x = serverState.messages.concat({message: 'User '+parsedUser.username+ ' has joined the chat!'})
+        serverState.messages = x
+        let success = {out: 'success', id: sessionID, msgs: serverState.messages }
+        res.send(JSON.stringify(success))
+    }
+    res.send(JSON.stringify('STOP'))
+
+})
+
+app.post('/submit', (req, res)=>{
+    let parsedMsg = JSON.parse(req.body.toString())
+    if (!parsedMsg.id||parsedMsg.id!==serverState.livesession[parsedMsg.username]){
+        res.send('PLEASE LEAVE')
+    }
+    else{
+    let x = serverState.messages.concat(parsedMsg)
+    console.log(serverState.messages)
+    serverState.messages=x
+    res.send(JSON.stringify(serverState.messages))}
+})
+
+app.get('/messages', (req, res)=>{
+    res.send(JSON.stringify(serverState.messages))
+})
+
+app.listen(4000, ()=>{
+    console.log('listening 4k')
+})
---
+++ frontend/src/App.js
@@ -0,0 +1,194 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      username: undefined,
+      textinput: "",
+      messages: [],
+      loggedin: false,
+      sessionID: undefined,
+      enterchat: false,
+      actives: []
+    };
+  }
+  componentDidMount = () => {
+    setInterval(
+      ()=>{
+      fetch("/messages")
+        .then(x => x.text())
+        .then(y => {
+          let parsedY = JSON.parse(y);
+          this.setState({ messages: parsedY })});
+      fetch("/activeUsers")
+                .then(x => x.text())
+                .then(y => {
+                  let parsedY = JSON.parse(y);
+                  this.setState({ actives: parsedY });
+                  if(this.state.loggedin){
+                    this.updateScroll()
+                  }
+                })}
+        ,
+      600
+    );
+         
+    
+  };
+
+  updateScroll = ()=>{
+    var element = document.getElementById("chat");
+    element.scrollTop = element.scrollHeight;
+}
+
+  handleSubmit = e => {
+    e.preventDefault();
+    fetch("/submit", {
+      method: "POST",
+      body: JSON.stringify({
+        username: this.state.username,
+        message: this.state.textinput,
+        id: this.state.sessionID,
+        time: new Date().getTime()
+      })
+    })
+      .then(x => x.text())
+      .then(y => {
+        let parsedY = JSON.parse(y);
+        this.setState({ messages: parsedY, textinput: "" });
+      });
+      this.updateScroll();
+    document.getElementById("inp").value = "";
+  };
+
+  handleChange = e => {
+    let input = e.target.value;
+    this.setState({ textinput: input });
+  };
+
+  
+
+  handleCreate = e => {
+    e.preventDefault();
+    let name = document.getElementById("creNam").value;
+    let pass = document.getElementById("crePas").value;
+    if (pass.length > 3 && name.length > 3) {
+      fetch("/create", {
+        method: "POST",
+        body: JSON.stringify({ username: name, password: pass })
+      })
+        .then(x => x.text())
+        .then(y => {
+          y === "success" ? alert("Account created") : alert("Username Taken");
+        });
+    } else {
+      alert("PASSWORD OR USERNAME TOO SHORT");
+    }
+    document.getElementById("creNam").value = "";
+    document.getElementById("crePas").value = "";
+  };
+
+  handleLogin = e => {
+    e.preventDefault();
+    let name = document.getElementById("username").value;
+    let pass = document.getElementById("pass").value;
+    if (pass && name) {
+      fetch("/login", {
+        method: "POST",
+        body: JSON.stringify({ username: name, password: pass })
+      })
+        .then(x => x.text())
+        .then(y => {
+          let parsedY = JSON.parse(y);
+          if (parsedY.out === "success") {
+            this.setState({
+              loggedin: true,
+              sessionID: parsedY.id,
+              username: name,
+              messages: parsedY.msgs
+            })
+            this.updateScroll();
+          ;
+          } else {
+            alert("INCORRECT");
+            document.getElementById("pass").value = '';
+            document.getElementById("username").value = '';
+          }
+        });
+    } else {
+      alert("INCORRECT")
+      document.getElementById("pass").value = "";
+      document.getElementById("username").value = "";
+    }
+  };
+
+  render() {
+    if (!this.state.loggedin) {
+      return <div className="body">
+          <div className="topbar">RandyChat</div>
+          <div className="login">
+            <div className="header" style={{ fontSize: "13pt" }}>
+              LOG IN?
+            </div>
+
+            <form onSubmit={this.handleLogin}>
+              USERNAME:<br />
+              <input type="text" id="username" placeholder="USERNAME" />
+              <br />PASSWORD:<br />
+              <input type="password" id="pass" placeholder="PASSWORD" />
+              <br />
+              <input className="logbut" type="submit" />
+            </form>
+          </div>
+          <div className="create">
+            <div className="header" style={{ fontSize: "13pt" }}>
+              CREATE ACCOUNT?
+            </div>
+            <form onSubmit={this.handleCreate}>
+              USERNAME:<br />
+              <input type="text" id="creNam" placeholder="USERNAME" />
+              <br />PASSWORD:<br />
+              <input type="password" id="crePas" placeholder="PASSWORD" />
+              <br />
+              <input className="logbut" type="submit" />
+            </form>
+          </div>
+        </div>;
+    } else {
+      return <div className="body">
+      <div className='topbar'>RandyChat</div>
+          <div className="chat">
+            <div className="top" id="chat">
+              <ul>
+                {this.state.messages.map((x, k) => (
+                  <li key={k}>
+                    {x.username
+                      ? x.username + ": " + x.message
+                      : "" + x.message}
+                  </li>
+                ))}
+              </ul>
+            </div>
+
+            <div className="bottom">
+              <form onSubmit={this.handleSubmit}>
+                <input className="chatbar" type="text" onChange={this.handleChange} id="inp" />
+                <input type="submit" />
+              </form>
+            </div>
+          </div>
+          <div className="actives">
+            <ul >
+              <div className='header'>ACTIVE USERS</div>
+              {this.state.actives.map((x, k) => <li key={k}>{x}</li>)}
+            </ul>
+          </div>
+        </div>;
+    }
+  }
+}
+
+export default App;
